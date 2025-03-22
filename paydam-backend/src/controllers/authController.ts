4ba0c33aa20348e27b50ec03a8f3cb84bc163447
Samiuddin <mdsamiuddin2005@outlook.com>
2025-03-23 00:26:13 +0530

    chore : not many changes made

--- paydam-backend/src/controllers/authController.ts
+++ paydam-backend/src/controllers/authController.ts
@@ -1,5 +1,7 @@
 import { Express } from "express";
 import { Request , Response } from "express";
+import bcrypt from "bcryptjs"
+import { registerModel } from "../models/authModel";
 export const AuthController = {
     // the plan is make the user signin using his email and pasword then it will get a 2FA 
     LoginUser : async (req:Request , res:Response) => {
@@ -7,12 +9,26 @@ export const AuthController = {
         
 
     },
-    RegisterUser : async (req : Request , res:Response) => {
-        const {full_name ,email , password , phone_number } = req.body ;
-        
-
-        // lets make it as when the user logs for the first time when he created his account we give him a random accountnumber
-
-     }
+    RegisterUser: async (req: Request, res: Response) => {
+        try {
+            const { fullName, email, password, phoneNumber } = req.body;
+            // Hash the user's password
+            const hashedPassword = await bcrypt.hash(password, 10);
+   
+            // Generate a random 10-digit account number
+            const accountNumber = Math.floor(1000000000 + Math.random() * 9000000000);
+    
+            // Save user to database
+            await registerModel(fullName, email, hashedPassword, phoneNumber, accountNumber);
+           
+            res.status(201).json({
+                message : "User registered successfully",
+                user : {fullName , email , phoneNumber , accountNumber}
+            })
+        } catch (error) {
+            console.error("Error in RegisterUser:" , error);
+            res.status(500).json({error : "Internal error occured"})
+        }
+    }
     
 }
---
+++ paydam-backend/src/models/authModel.ts
@@ -0,0 +1,3 @@
+export async function registerModel (fullName:string,email:string , password:string , phoneNumber:number , accountNumber:number)  {
+    
+}
