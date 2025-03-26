34ef3dfe1401947f05bb9cd2a7ad60378985d71e
Samiuddin <mdsamiuddin2005@outlook.com>
2025-03-26 23:58:41 +0530

    chore : add react router , tailwind

---
+++ payDam-frontend/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      this is teh login page 
+    </div>
+  )
+}
+
+export default Login
---
+++ payDam-frontend/src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      this is the register pae
+    </div>
+  )
+}
+
+export default Register
